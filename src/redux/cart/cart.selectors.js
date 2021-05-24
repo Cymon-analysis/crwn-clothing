06decd5ad6b5ffb9b1a2dc077dae52fd1964caf2
Cymon-analysis <simon.gay@converteo.com>
2021-05-24 22:09:01 +0200

    selector fixes

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,13 +1,19 @@
-import { createSelector } from 'reselect'; 
+  
+import { createSelector } from 'reselect';
 
-const selectCart = state => state.cart; 
+const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
-    [selectCart], 
-    (cart) => cart.cartitems
+  [selectCart],
+  cart => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
-    [selectCartItems], 
-    cartItems => cartItems.reduce((accumulatedQuantity, cartItems) => accumulatedQuantity + cartItems.quantity , 0)
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
 );
